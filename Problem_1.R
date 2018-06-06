6d6eaf60dfba8dad0cfa90024e17597021dbc2b0
Aleksandar Kuyumdzhiev <akuyumdzhiev@yahoo.com>
2018-06-06 20:18:46 +0300

    700828, 700850

--- Problem_1.R
+++ Problem_1.R
@@ -3,25 +3,58 @@
 library(dplyr)
 
 ## Read the data
-houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
+houseWork <-
+  read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
 str(houseWork)
 
 ## a)
 
+summary(houseWork)
+
 ## b)
+
+muf <- mean(subset(houseWork$hours, houseWork$sex == 'f'))
+mum <- mean(subset(houseWork$hours, houseWork$sex == 'm'))
 
 ## c)
 
+houseWork <- within(houseWork, {
+  female <- ifelse(sex == 'f', 'TRUE', 'FALSE')
+  male <- ifelse(sex == 'm', 'TRUE', 'FALSE')
+})
+
+houseWork
+
 ## d)
+
+fit <- lm(hours ~ female, data = houseWork)
+summary(fit)
 
 ## e)
 
+##Intercept ist Schätzer für b0
+##femaleTRUE ist Schätzer für b1, auch die Neigung
+##Die beide werden Regressionskoeffizienten genannt
+
 ## f)
+
+##The null hypothesis states, that the average house-work hours for women is
+##less or equal to the house-work for men. The alternative states, that the house-work for women
+##is greater than the house-work for men.
 
 ## g)
 
+T <- (-14.4555 - 32.8138) / 0.3186
+
+pt(-T, df = 2)
+
 ## h)
+
+##We do not reject the null hypothesis, because the P-value is greater than 0.05
 
 ## i)
 
 ## j)
+
+fit1 <- lm(hours ~ female + male, data = houseWork)
+summary(fit1)
--- Problem_2.R
+++ Problem_2.R
@@ -1,13 +1,48 @@
 ## Homework 4, Problem 2
-crime <- read.delim('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv', stringsAsFactors = FALSE)
+##install.packages(c('dplyr', 'ggplot2'))
+
+library(dplyr)
+library(ggplot2)
+
+crime <-
+  read.delim(
+    'https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv',
+    stringsAsFactors = FALSE
+  )
 str(crime)
 
 ## a)
 
+fit <- lm(C ~ HS, data = crime)
+summary(fit)
+
+##Intercept ist Schätzer für b0
+##HS ist Schätzer für b1, auch die Neigung
+##Die beide werden Regressionskoeffizienten genannt
+
 ## b)
+
+pairs( ~ C + U + I + HS, data = crime)
 
 ## c)
 
+fit1 <- lm(C ~ HS + U, data = crime)
+summary(fit1)
+
+##Intercept ist Schätzer für b0
+##HS ist Schätzer für b1, auch die Neigung
+##U ist Schätzer für b2
+##With U in the model the value of the Estimated coefficient
+##becomes less than withou U in the model.
+
 ## d)
 
+pairs( ~ C + HS, data = crime)
+
+##The politician's claims that the lower education spending will reduce the crime rates are wrong.
+##We can see from the scatterplot matrix, that the lower HS bring more crimes and vice versa.
+
 ## e)
+
+fit3 <- lm(C ~ HS + U + I, data = crime)
+summary(fit3)
